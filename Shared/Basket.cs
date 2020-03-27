78a504d9bba08b4ef77e18805d96368713c04ecd
Anatoliy Novikov <chamskra66er@gmail.com>
2020-03-27 11:55:20 +0300

    added images

--- Shared/Basket.cs
+++ Shared/Basket.cs
@@ -9,5 +9,9 @@ namespace NewBlazor.Shared
         public Customer Customer { get; set; } = new Customer();
         public List<int> Orders { get; set; } = new List<int>();
         public bool HasPaid { get; set; } = false;
+        public void Add(int pizzaId)
+        {
+            Orders.Add(pizzaId);
+        }
     }
 }
