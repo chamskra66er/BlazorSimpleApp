a809efca73e227388c1aeeaea54d26d9964ae897
Anatoliy Novikov <chamskra66er@gmail.com>
2020-03-27 10:38:33 +0300

    adde UI

---
+++ Shared/UI.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewBlazor.Shared
+{
+    public class UI
+    {
+        public bool ShowBasket { get; set; } = true;
+    }
+}
