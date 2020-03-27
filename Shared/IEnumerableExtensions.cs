79c835e4f8f4560a949f65957b3e0bf19e884a63
Anatoliy Novikov <chamskra66er@gmail.com>
2020-03-27 20:15:49 +0300

    changed view index

---
+++ Shared/IEnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace NewBlazor.Shared
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool Any(this IEnumerable enumerable)
+        {
+            return enumerable.GetEnumerator().MoveNext() == true;
+        }
+    }
+}
