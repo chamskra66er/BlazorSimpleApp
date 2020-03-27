6f14aec7bc5c9bef89a3b6ae1bedf8aff2a1f68b
Anatoliy Novikov <chamskra66er@gmail.com>
2020-03-27 10:40:53 +0300

    add State

---
+++ Shared/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewBlazor.Shared
+{
+    public class State
+    {
+        public Menu Menu { get; set; } = new Menu();
+        public Basket Basket { get; set; } = new Basket();
+        public UI UI { get; set; } = new UI();
+    }
+}
